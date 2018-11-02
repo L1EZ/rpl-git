b53e11e62b1036051768793619356b3ac82d7a71
CAHYADI HARTANTO SULAYMAN <i16036@ftis>
2018-11-02 13:44:08 +0700

    fungsi plus dan minus

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,27 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @author Cahyadi Hartanto 2016730036
+ * @author Gunawan Chris 2016730011
  */
 
 /**
- *
- * @author i16036
+ * Kelas ini berguna untuk melakukan proses aritmatika.
  */
 public class Calculator {
+    /**
+     * Constructor kelas Calculator
+     */
     public Calculator(){
     
         
     }
     
+    /**
+     * Method ini berguna untuk melakukan proses perjumlahan a dengan b
+     * 
+     * @param a merupakan angka yang ingin di tambah dengan b
+     * @param b merupakan angka yang akan ditambah ke a
+     * @return jumlah a dengan b
+     */
     public float plus(float a, float b){
-        
-        
+        return a+b;
     }
     
+    /**
+     * Method ini berguna untuk melakukan proses pengurangan a dengan b
+     * 
+     * @param a merupakan angka yang ingin di kurangi oleh b
+     * @param b merupakan angka yang akan mengurangi a
+     * @return hasil pengurangan a dengan b
+     */
     public float minus(float a, float b){
-        
-        
+        return a-b;
     }
     
     public float multiply(float a, float b){
